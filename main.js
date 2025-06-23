73ae36f7b206bc629a0ffb996d9b7095c51df5a4
dudinha <137805481+duda008@users.noreply.github.com>
2025-06-23 10:42:53 -0300

    aula

---
+++ main.js
@@ -0,0 +1,16 @@
+const botoes = document.querySelectorAll(".botoes");
+const textos = document.querySelectorAll(".abas.conteudo");
+const contador = document.querySelectorAll(".contador")
+console.log(botoes)
+
+for(let i=0; i< botoes.length; i++){
+    botoes[i].onclick = function(){
+        
+        for (let j=0; j<botoes.length; j++){
+            botoes[j].remove.add("ativo");
+            textos[j].remove.add("ativo");
+        }
+        botoes[i].classList.add("ativo");
+        textos[i].classList,add("ativo");
+    }
+}
